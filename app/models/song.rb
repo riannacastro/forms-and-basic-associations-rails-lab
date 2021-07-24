f468fed71c64dd88e1fcbdbe668270ec83169545
Rianna <riannaxcastro@gmail.com>
2021-07-23 19:24:20 -1000

    commit tree 4a9e2a09dfd5a911678a5fe6c3f4621dd742c694

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,27 @@
 class Song < ActiveRecord::Base
-  # add associations here
+ belongs_to :artist
+ belongs_to :genre
+ has_many :notes
+
+ def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+    self.save
+ end
+
+ def artist_name
+  self.artist.name if self.artist
+ end
+
+ def note_content=(contents)
+    contents.each do |c|
+      if !c.blank?
+        self.notes.build(content: c)
+      end
+    end
+  end
+  
+
+  def note_content
+  end
+
 end
